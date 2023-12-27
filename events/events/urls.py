ab39856bdd9d6f0a8b618fb84257d64c4627cbe7
zhuravlev_work <855434@gmail.com>
2023-12-27 08:44:14 +0300

    delete ck5

--- events/events/urls.py
+++ events/events/urls.py
@@ -17,8 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from django.conf.urls.static import static
-from django.conf import settings
+
 
 urlpatterns = [
                   path('ckeditor5/', include('django_ckeditor_5.urls')),
@@ -26,6 +25,3 @@ urlpatterns = [
                   path('', include('event.urls', namespace='event'))
               ]
 
-if settings.DEBUG:
-    urlpatterns = [path('__debug__/', include('debug_toolbar.urls'))] + urlpatterns
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
