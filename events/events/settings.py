c1d7232f6c446abe7fba7ae842ef9493c3b2bbf8
zhuravlevivan <855434@gmail.com>
2024-01-06 16:30:53 +0300

    test: test for coolify

--- events/events/settings.py
+++ events/events/settings.py
@@ -11,25 +11,20 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
-import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
 
-environ.Env.read_env(BASE_DIR / '.env')
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-%l-kkux6q9wcap$tqj#_q1th9wck^q859#+panhz!2b1ch@0&!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = True
 
-ALLOWED_HOSTS = env('ALLOWED_HOSTS').split()
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
 
 # Application definition
 
